6946a8043bc746abf8e9ed64e056eb9379b4c30e
sjadala <sravijadala@gmail.com>
2019-01-08 12:33:12 -0500

    please help me the logic for this program

--- try.py
+++ try.py
@@ -1,3 +1,11 @@
-print("iam trying how to use github")
-print("iam try to add content from my meachine")
+#print("iam trying how to use github")
+#print("iam try to add content from my meachine")
+#7. Write the higher order function reduce to calculate the total sum of first 30 odd values in the range of 1 to 100. 
+
+import functools
+res = list(filter(lambda x: x % 2 == 1, list(range(0, 101))))
+print(res)
+res = functools.reduce(lambda x, y: x + y, filter(lambda x: x % 2 == 1, range(1, 100)))
+print(res)
+
 
